dde60d88dfc2a91857c504dbef689116c6ac4ff7
Corey Porter <coreyaporter@gmail.com>
2024-12-09 08:28:37 -0800

    day 7 part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -22,27 +22,82 @@ fn parse_line(line: String) -> Option<Problem> {
     })
 }
 
-fn solvable_s(s: &[i64], target: i64) -> bool {
+fn solvable_s_1(s: &[i64], target: i64) -> bool {
     match s {
         [] => false,
         [x] => *x == target,
         [first, rest @ ..] => {
-            (target % first == 0 && solvable_s(rest, target / first))
-                || solvable_s(rest, target - first)
+            (target % first == 0 && solvable_s_1(rest, target / first))
+                || solvable_s_1(rest, target - first)
         }
     }
 }
 
-fn solvable(p: &Problem) -> bool {
-    return solvable_s(&p.elements.as_slice(), p.target);
+fn solvable_1(p: &Problem) -> bool {
+    solvable_s_1(&p.elements.as_slice(), p.target)
+}
+
+fn ends_with_same_digits(aa: i64, bb: i64) -> bool {
+    let mut a = aa;
+    let mut b = bb;
+    while a > 0 && b > 0 {
+        if a % 10 != b % 10 {
+            return false;
+        }
+        a /= 10;
+        b /= 10;
+    }
+    return true;
+}
+
+fn truncate_digits(aa: i64, bb: i64) -> i64 {
+    let mut a = aa;
+    let mut b = bb;
+    while a > 0 && b > 0 {
+        a /= 10;
+        b /= 10;
+    }
+    return a;
+}
+
+fn solvable_s_2(s: &[i64], target: i64) -> bool {
+    match s {
+        [] => false,
+        [x] => *x == target,
+        [first, rest @ ..] => {
+            (target % first == 0 && solvable_s_2(rest, target / first))
+                || (ends_with_same_digits(target, *first)
+                    && solvable_s_2(rest, truncate_digits(target, *first)))
+                || solvable_s_2(rest, target - first)
+        }
+    }
+}
+
+fn solvable_2(p: &Problem) -> bool {
+    solvable_s_2(&p.elements.as_slice(), p.target)
 }
 
 fn main() {
     let problems: Vec<Problem> = read_input().map(parse_line).filter_map(|x| x).collect();
     let part1 = problems
         .iter()
-        .filter(|p| solvable(p))
+        .filter(|p| solvable_1(p))
+        // .map(|p: &Problem| {
+        //     println!("{} = {:?}", p.target, p.elements);
+        //     p
+        // })
         .map(|p| p.target)
         .sum::<i64>();
     println!("part 1: {}", part1);
+
+    let part2 = problems
+        .iter()
+        .filter(|p| solvable_2(p))
+        // .map(|p: &Problem| {
+        //     println!("{} = {:?}", p.target, p.elements);
+        //     p
+        // })
+        .map(|p| p.target)
+        .sum::<i64>();
+    println!("part 2: {}", part2);
 }
