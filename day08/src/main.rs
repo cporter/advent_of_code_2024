da555bae5ee14c20296a91e8d89b23c0b844e4a3
Corey Porter <coreyaporter@gmail.com>
2024-12-13 19:39:05 -0800

    day 8 part 1

---
+++ day08/src/main.rs
@@ -0,0 +1,62 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+use util::read_input;
+
+type Grid = Vec<Vec<char>>;
+type Letter = (char, i32, i32);
+
+fn find_letters(g: &Grid) -> Vec<Letter> {
+    let mut letters: Vec<Letter> = g
+        .iter()
+        .enumerate()
+        .flat_map(move |(ri, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(ci, letter)| (*letter, ri as i32, ci as i32))
+        })
+        .filter(|(letter, _, _)| *letter != '.')
+        .collect();
+    letters.sort();
+    letters
+}
+
+fn main() {
+    let grid: Grid = read_input()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect();
+    let m = grid.len() as i32;
+    let n = grid[0].len() as i32;
+    let letters = find_letters(&grid);
+    let mut stations: HashSet<(i32, i32)> = HashSet::new();
+
+    let grouped = letters.into_iter().chunk_by(|&(ch, _, _)| ch); // Group by the char value
+
+    // Iterate over the grouped results
+    for (_, group) in &grouped {
+        let chunks: Vec<(char, i32, i32)> = group.into_iter().collect();
+        let pairs: Vec<((i32, i32), (i32, i32))> = chunks
+            .iter()
+            .flat_map(|(_, x1, y1)| {
+                chunks.iter().filter_map(move |(_, x2, y2)| {
+                    if (x1, y1) != (x2, y2) {
+                        Some(((*x1, *y1), (*x2, *y2)))
+                    } else {
+                        None
+                    }
+                })
+            })
+            .collect();
+        for ((x1, y1), (x2, y2)) in pairs {
+            let dx = x2 - x1;
+            let dy = y2 - y1;
+            stations.insert((x1 - dx, y1 - dy));
+            stations.insert((x2 + dx, y2 + dy));
+        }
+    }
+
+    let part1 = stations
+        .iter()
+        .filter(|(x, y)| x >= &0 && y >= &0 && x < &n && y < &m)
+        .count();
+    println!("part 1: {}", part1);
+}
